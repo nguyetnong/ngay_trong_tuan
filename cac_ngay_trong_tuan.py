360843f3b48757eb2ebf22b29190aa788131a292
nguyetnong <nguyetnong71099@gmail.com>
2022-10-23 18:44:58 +0700

    cac ngay trong tuan

---
+++ cac_ngay_trong_tuan.py
@@ -0,0 +1,17 @@
+num = int(input("nhap vao cac so  (form 1 to 7): "))
+if num == 2:
+    print("Monday")
+elif num == 3:
+    print("Tuesday")
+elif num == 4:
+    print("Wednesday")
+elif num == 5:
+    print("Thursday")
+elif num == 6:
+    print("Friday")
+elif num == 7:
+    print("Saturday")
+elif num == 1:
+    print("Sunday")
+else:
+    print(" khong phai thu trong tuan")
